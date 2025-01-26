197e46896af754ab7b07fa42011be8c1dd35dd7a
Rudra <rudra.phymos@gmail.com>
2025-01-26 14:54:52 +0530

    users post api updated

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
     // Endpoint: Get all users
     app.post('/users', async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        res.send({ message: 'User already exists' });
+        return;
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
